df88c497262bc1aa3014a32d2cfa8dba211da31d
Samuel Sarette <samuelsarette@linux.com>
2020-02-01 21:16:59 -0500

    Update Results screen code

--- Shipwreck/Assets/Scripts/ResultScreenManager.cs
+++ Shipwreck/Assets/Scripts/ResultScreenManager.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Shipwreck;
 
 public class ResultScreenManager : MonoBehaviour
 {
     
-	public WorldBuilder worldInfo;
+	public IWorld _world;
 	public Text resultText;
     
 	public Text countdownText;
@@ -15,16 +16,19 @@ public class ResultScreenManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var result = worldInfo.World.State.Ship.TotalHealth > 50.0f ? "survived" : "been destroyed";
+        // Grab the world from the builder
+        _world = GameObject.FindObjectOfType<WorldBuilder>().World;
+        
+        var result = _world.State.Ship.TotalHealth > 50.0f ? "survived" : "been destroyed";
         resultText.text = $"Result:\nThe Station has {result}";
     }
 
     // Update is called once per frame
     void Update()
     {
-        countdownText.text = $"Loading in {worldInfo.World.State.RemainingTime:#}s";
+        countdownText.text = $"Loading in {_world.State.RemainingTime:#}s";
 
-        if (worldInfo.World.State.Mode != Shipwreck.WorldData.GameMode.Finished) 
+        if (_world.State.Mode != Shipwreck.WorldData.GameMode.Finished) 
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene(0); // return to main
             return;
